cd9f36a450d1926f46376e4b0f3bf9b31002a4c9
Goni <goni@Gonis-MacBook-Air-7.local>
2026-09-21 15:12:20 +0100

    Bump grounded formula to v0.13.0

--- Formula/grounded.rb
+++ Formula/grounded.rb
@@ -3,8 +3,8 @@ class Grounded < Formula
 
   desc "Find dangling references in code comments. Deterministic, offline, zero dependencies"
   homepage "https://github.com/gonisulaimann/Grounded"
-  url "https://files.pythonhosted.org/packages/31/c0/650a14c073f24b2bffc52487ba1a9c6d117a1de8933d24b52f1410eb1414/grounded_lint-0.12.1.tar.gz"
-  sha256 "15c1b31c98ff3b54dbdb1823232d3599ec4ec77799fc0bc8909e70c4c70d89ad"
+  url "https://files.pythonhosted.org/packages/78/8f/dc82eb6190142c43bbba357edcd9e00f35f986873e5b4ac50522d01956b2/grounded_lint-0.13.0.tar.gz"
+  sha256 "7c1353b8a03b271d58a5d4aa67557e7d7e34a2b671fbf61eaaf200a6441530b5"
   license "MIT"
 
   depends_on "python@3.13"
